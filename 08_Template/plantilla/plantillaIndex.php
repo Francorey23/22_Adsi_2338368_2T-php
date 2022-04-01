79f9a63eb3fd1e70bd555adccaedc7326d976ac0
Francorey23 <francorey23@gmail.com>
2022-04-01 13:40:09 -0500

    Respaldo de Proyecto Turismo

--- 08_Template/plantilla/plantillaIndex.php
+++ 08_Template/plantilla/plantillaIndex.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Bootstrap CSS -->
-  <link href="../css/estiloFormulario.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
@@ -16,11 +16,7 @@
   <title>Hoteleria y turismo Popayán 2022</title>
  
 </head>
-<style>
-    i{
-      font-size: 20px;
-     }
-  </style>
+
 <body>
 <div class="formulario">
 
